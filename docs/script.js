3193efb1c67737c9676eb21228dffdc55f802eff
Theodore Brzustowicz <tbrzustowicz@icloud.com>
2026-07-21 20:19:30 -0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -33,8 +33,8 @@ async function loadData() {
         const beforebabyCount = Number(latest[2]);
         const beforeadultCount = Number(latest[3]);
 
-        const babycount = beforebabyCount * 2;
-        const adultcount = beforeadultCount * 2;
+        const babyCount = beforebabyCount * 2;
+        const adultCount = beforeadultCount * 2;
         
         const temperature = latest[5];
         const weather = latest[6] || "";
